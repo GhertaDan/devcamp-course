1af67bf52b044179b56b9a135871b3bcf2c193b2
GhertaDan <deepdanny2003@gmail.com>
2022-08-03 22:02:58 +0300

    devcamp project added

---
+++ script.js
@@ -0,0 +1,4 @@
+function fullName(firstName, lastName, language) {
+    var language = language || 'English';
+    return firstName.toUpperCase() + "", "" + lastName.toUpperCase() + "" - "" + language;
+} 
